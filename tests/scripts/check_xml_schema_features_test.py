0fe0b7fe8486652ca1b79a1e7743782edce6e1ba
Juuso Korvuo <juuso.korvuo@csc.fi>
2019-08-21 15:41:36 +0300

    KDKPAS-2090 Add schema and schematron unit tests

---
+++ tests/scripts/check_xml_schema_features_test.py
@@ -0,0 +1,46 @@
+"""
+Test that check-xml-schema-features script behaves correctly
+when the input is:
+    - a valid METS file
+    - an invalid METS file
+    - not an xml file
+"""
+import os
+import pytest
+
+from ipt.scripts.check_xml_schema_features import main
+from tests.testcommon import shell
+from tests.testcommon.settings import TESTDATADIR
+
+
+@pytest.mark.parametrize(
+    ['filename', 'expected'],
+    [
+        ('valid_1.7.1_mets.xml',
+         {'returncode': 0,
+          'stdout': 'Success',
+          'stderr': None}),
+        ('invalid_1.7.1_mets.xml',
+         {'returncode': 117,
+          'stdout': None,
+          'stderr': 'invalid_1.7.1_mets.xml fails to validate'}),
+        ('invalid__not_xml.txt',
+         {'returncode': 117,
+          'stdout': None,
+          'stderr': 'invalid__not_xml.txt does not appear to be XML'})
+    ])
+def test_check_xml_schema_features(filename, expected):
+    """
+    Run each test case and check that returncode, stdout and
+    stderr match the expected.
+    """
+    args = [os.path.join(TESTDATADIR, 'xml', filename)]
+    (returncode, stdout, stderr) = shell.run_main(main, args)
+
+    if expected['stderr']:
+        assert expected['stderr'] in stderr
+    else:
+        assert not stderr
+    if expected['stdout']:
+        assert expected['stdout'] in stdout
+    assert expected['returncode'] == returncode
---
+++ tests/scripts/check_xml_schematron_features_test.py
@@ -0,0 +1,59 @@
+"""
+Test that check-xml-schematron-features script behaves correctly
+when the input is:
+    - a valid METS file
+    - an invalid METS file
+    - not an xml file
+"""
+import os
+import lxml.etree
+import pytest
+
+from ipt.scripts.check_xml_schematron_features import main
+from tests.testcommon import shell
+from tests.testcommon.settings import TESTDATADIR
+
+
+def is_parsable_xml(string):
+    """Helper to check if string can be parsed as xml."""
+    try:
+        lxml.etree.fromstring(string)
+        return True
+    except lxml.etree.XMLSyntaxError:
+        return False
+
+
+@pytest.mark.parametrize(
+    ['filename', 'expected'],
+    [
+        ('valid_1.7.1_mets.xml',
+         {'returncode': 0,
+          'stderr': None}),
+        ('invalid_1.7.1_mets.xml',
+         {'returncode': 117,
+          'stderr': None}),
+        ('invalid__not_xml.txt',
+         {'returncode': 117,
+          'stderr': 'invalid__not_xml.txt does not appear to be XML'})
+    ])
+def test_check_xml_schematron_features(filename, expected):
+    """
+    Run each test case and check that
+        - stderr contains the correct error or is empty if no
+          errors are expected
+        - stdout can be parsed as xml (so it can be included in
+          the validation report element 'eventOutcomeDetailExtension')
+        - return code is what is expected
+    """
+    mets_path = os.path.join(TESTDATADIR, 'xml', filename)
+    schema_path = '/usr/share/dpres-xml-schemas/schematron/mets_audiomd.sch'
+    args = ['-s', schema_path, mets_path]
+    (returncode, stdout, stderr) = shell.run_main(main, args)
+
+    if expected['stderr']:
+        assert expected['stderr'] in stderr
+    else:
+        assert not stderr
+    if stdout:
+        assert is_parsable_xml(stdout)
+    assert expected['returncode'] == returncode
