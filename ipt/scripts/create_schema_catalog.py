a5b20e8d3dd7c1cd207389aca3cc0dc013d9f9a8
Thien Tran <thien.tran@csc.fi>
2021-01-15 06:59:58 +0200

    SAPA-526 Create new script to create XML schema catalog

---
+++ ipt/scripts/create_schema_catalog.py
@@ -0,0 +1,121 @@
+"""Create a schema catalog from given parameters."""
+
+from __future__ import print_function, unicode_literals
+
+import argparse
+import os
+import sys
+from six.moves.urllib.parse import unquote_plus, urlparse
+
+import mets
+import premis
+import xml_helpers.utils
+from xml_helpers.schema_catalog import construct_catalog_xml
+
+
+def main():
+    """ The main method for create-schema-catalog script"""
+    args = parse_arguments()
+    _create_schema_catalog(mets=args.mets,
+                           sip=args.sip,
+                           output=args.output,
+                           catalog=args.catalog)
+    return 0
+
+
+def parse_arguments():
+    """ Create arguments parser and return parsed command line arguments"""
+    default_path = (
+        '/etc/xml/dpres-xml-schemas/schema_catalogs/catalog_main.xml')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('mets',
+                        metavar='METS',
+                        help='File path to read given METS file for schemas')
+    parser.add_argument('sip',
+                        metavar='SIP',
+                        help='Path to information package')
+    parser.add_argument('output',
+                        metavar='OUTPUT',
+                        help='Write the catalog to given file path')
+    parser.add_argument(
+        '-c', '--catalog',
+        dest='catalog',
+        default=default_path,
+        help='File path to another catalog to add to current schema catalog')
+
+    return parser.parse_args()
+
+
+def _create_schema_catalog(mets, sip, output, catalog):
+    """Create schema catalog based on given METS XML, and possibly catalog
+    file.
+
+    :param mets: METS XML file to fetch the schema URIs.
+    :param sip: SIP path where all package content is located under.
+    :param output: File to create the schema catalog to.
+    :param catalog: Catalog file to be added as nextCatalog entry.
+    :return:
+    """
+
+    schemas = _collect_xml_schemas(sip_path=sip,
+                                   mets_filepath=mets)
+    catalog_xml = construct_catalog_xml(
+        base_path=sip,
+        rewrite_rules=schemas,
+        next_catalogs=[catalog])
+    with open(output, 'wb') as out_file:
+        out_file.write(xml_helpers.utils.serialize(catalog_xml))
+
+
+def _collect_xml_schemas(sip_path, mets_filepath):
+    """Collect all XML schemas from the METS. The schemas are ordered as
+    a dictionary with the schemaLocations as keys and the schema paths
+    as values. The schemaLocations can either be URIs or paths to
+    files.
+
+    The XML schema catalog mainly understands URIs when mapping
+    locations to paths. If a local file path is used without an URI
+    syntax, the catalog needs to read the file paths as relative
+    locations from the catalog file itself in order to rewrite the
+    URI prefixes properly.
+
+    :param sip_path: The path to the SIP contents
+    :param mets_filepath: METS XML filepath.
+    :returns: a dictionary of schema locations and paths
+    """
+    mets_tree = xml_helpers.utils.readfile(mets_filepath)
+    schemas = {}
+    environments = None
+    for techmd in mets.iter_techmd(mets_tree):
+        environments = premis.iter_environments(techmd)
+    for environment in premis.environments_with_purpose(environments,
+                                                        purpose='xml-schemas'):
+        for dependency in premis.parse_dependency(environment):
+            parsed_name = next(premis.iter_elements(dependency,
+                                                    'dependencyName')).text
+
+            # Schema_path as unquoted file path with leading slashes removed
+            # since schema_path should always be a relative path
+            schema_path = unquote_plus(urlparse(parsed_name).path.lstrip('/'))
+            # Check that illegal paths pointing outside the SIP don't exist,
+            # i.e. skip schemas with illegal paths
+            if not os.path.abspath(
+                    os.path.join(sip_path, schema_path)).startswith(
+                os.path.abspath(sip_path)):
+                continue
+
+            (_, id_value) = premis.parse_identifier_type_value(
+                dependency, prefix='dependency')
+            # Add absolute path to catalog file if the value is a simple
+            # file path and not an URI
+            if not urlparse(id_value).scheme:
+                schema_path = os.path.join(sip_path, id_value)
+            schemas[id_value] = schema_path
+
+    return schemas
+
+
+if __name__ == '__main__':
+    RETVAL = main()
+    sys.exit(RETVAL)
